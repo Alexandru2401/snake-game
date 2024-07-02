6a3720940c20849a2d0cf8e52502fb0a31c638dc
Alexandru2401 <a.toma231@yahoo.com>
2024-07-02 14:59:32 +0300

    updape - create keypress event and food function

--- index.js
+++ index.js
@@ -30,17 +30,90 @@ function drawSnake() {
 function advanceSnake() {
   const head = { x: snake[0].x + dx, y: snake[0].y + dy };
   snake.unshift(head);
-  snake.pop();
+  const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+  if (didEatFood) {
+    createFood();
+  } else {
+    snake.pop();
+  }
 }
 
-advanceSnake();
-dx = 0; // Horizontal velocity to 0
-dy = -10;
-advanceSnake();
-drawSnake();
 function clearCanvas() {
   ctx.fillStyle = "white";
   ctx.strokeStyle = "black";
   ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height);
   ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
 }
+
+function main() {
+  setTimeout(function onTick() {
+    clearCanvas();
+    drawFood();
+    advanceSnake();
+    drawSnake();
+    main();
+  }, 100);
+}
+
+// make snake controls
+function changeDirection(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+  const keyPressed = event.keyCode;
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
+document.addEventListener("keydown", changeDirection);
+
+//snake food
+function randomTen(min, max) {
+  return Math.round(((Math.random() * (max - min) + min) / 10) * 10);
+}
+function createFood() {
+  foodX = randomTen(0, gameCanvas.width - 10);
+  foodY = randomTen(0, gameCanvas.height - 10);
+  snake.forEach(function isFoodOnSnake(part) {
+    const foodIsOnSnake = part.x == foodX && part.y == foodY;
+    if (foodIsOnSnake) createFood();
+  });
+}
+
+function drawFood() {
+  ctx.fillStyle = "red";
+  ctx.strokeStyle = "darkred";
+  ctx.fillRect(foodX, foodY, 10, 10);
+  ctx.strokeRect(foodX, foodY, 10, 10);
+}
+
+// implementing snake grow
+// const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+// if(didEatFood){
+//   createFood();
+// } else {
+//   snake.pop();
+// }
+
+createFood();
+
+main();
